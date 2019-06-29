7980b6e547f43753a02620ff3761aae464805675
hoonkk <byul2sol@naver.com>
2019-06-29 13:33:18 +0900

    Add Reset/LapTime

--- 2. 스톱워치/StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ 2. 스톱워치/StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.stopwatch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.concurrent.timer
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private var time = 0; // 시간 계산 변수
     private var timerTask: Timer? = null; // Null 허용 Timer 타입 변수
     private var isRunning = false; // 현재 실행 중인지
+    private var lap = 1 // 몇 번째 랩인지
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +27,14 @@ class MainActivity : AppCompatActivity() {
             } else {
                 pause()
             }
+        }
+
+        lapButton.setOnClickListener {
+            recordLapTime()
+        }
+
+        resetFab.setOnClickListener {
+            reset()
         }
     }
 
@@ -48,6 +58,33 @@ class MainActivity : AppCompatActivity() {
     {
         fab.setImageResource(R.drawable.ic_play_arrow_black_24dp)
         timerTask?.cancel() // 실행중인 타이머가 있다면 취소시킨다
+    }
+
+    private fun recordLapTime() // 랩 타임 기록
+    {
+        val lapTime = this.time // 현재 시간을 지역변수에 저장
+        val textView = TextView(this) // 동적으로 TextView 생성
+        textView.text = "$lap LAB : ${lapTime / 100}.${lapTime % 100}"
+
+        // 맨 위에 랩타임 추가
+        lapLayout.addView(textView, 0)
+        lap++
+    }
+
+    private fun reset() // 타이머 초기화
+    {
+        timerTask?.cancel() // 실행 중 타이머가 있으면 취소
+
+        // 모든 변수 초기화
+        time = 0
+        isRunning = false
+        fab.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+        secTextView.text = "0"
+        milliTextView.text="00"
+
+        // 랩 타임 초기화
+        lapLayout.removeAllViews()
+        lap = 1
     }
 
 }
